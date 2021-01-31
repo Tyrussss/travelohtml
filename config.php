0890f7d6f7cfe14c048e58e7c3fef013525491e2
Tyrussss <61288171+Tyrussss@users.noreply.github.com>
2021-02-01 01:29:10 +0700

    git add .

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PW', '');
+define('DB', 'traveling');
+
+define('ROOT', dirname(__FILE__));
+//echo __FILE__;
+define('BASE_URL', 'http://localhost/20210125');//CSS, JS, IMG
+define('IMG_PRODUCT', 'images/img_product');
+//echo '<br>'.ROOT;
